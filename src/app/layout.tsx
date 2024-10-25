599485530239c430b422560d6055891695d1427b
tedelcopp <tomas.edelcopp@gmail.com>
2024-10-25 17:52:43 -0300

    si

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 import "../styles/globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero"
 
 export const metadata = {
-  title: "Fitness Page",
-  description: "Página dedicada a clases de fitness.",
+  title: "Rugel Fitness",
+  description: "Mas que un entrenamiento",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Header /> 
-        {children} 
+        <Hero />
+        {children} {/* Aquí se renderizan las páginas específicas */}
         <Footer /> 
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from './components/About';
 import Classes from './components/Clasess';
 import Testimonials from './components/Testimonials';
+import Header from './components/Header';
 import Contact from './components/Contact';
 
 const Page = () => {
   return (
     <div>
+      <section id="Header"><Header /> </section>
       <section id="about"><About /></section>
       <section id="classes"><Classes /></section>
       <section id="testimonials"><Testimonials /></section>
